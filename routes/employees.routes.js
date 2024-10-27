112febb4a92f437fce4f9f6356ddd719b3c2528d
Patryk Bagan <patrykbn1997@gmail.com>
2024-10-27 15:23:53 +0100

    first commit

---
+++ routes/employees.routes.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const ObjectId = require('mongodb').ObjectId;
+
+router.get('/employees', (req, res) => {
+  req.db.collection('employees')
+  .find()
+  .toArray()
+  .then((data) => {
+    res.json(data);
+  })
+  .catch((err) => {
+    res.status(500).json({ message: err });
+  })
+});
+
+router.get('/employees/random', (req, res) => {
+  req.db.collection('employees')
+  .aggregate([{ $sample: { size: 1 }}])
+  .toArray()
+  .then((data) => {
+    res.json(data[0]);
+  })
+  .catch((err) => {
+    res.status(500).json({ message: err })
+  })
+});
+
+router.get('/employees/:id', (req, res) => {
+  req.db.collection('employees')
+  .findOne({ _id: ObjectId(req.parans.id)})
+  .then((data) => {
+    if(!data) res.status(404).json({ message: 'Not fdound' })
+      else res.json(data)
+  })
+  .catch((err) => {
+    res.status(500).json({ message: err })
+  })
+});
+
+router.post('/employees', (req, res) => {
+  const { firstName, lastName } = req.body;
+
+  req.db.collection('employees')
+  .insertOne({
+    firstName: firstName,
+    lastName: lastName,
+  })
+  .then(() => {
+    res.json({ message: 'OK'})
+  })
+  .catch((err) => {
+    res.status(500).json({ message: err })
+  })
+});
+
+router.put('/employees/:id', (req, res) => {
+  const { firstName, lastName } = req.body;
+
+  req.db.collection('employees')
+  .updateOne({ _id: ObjectId}, { $set: {firstName: firstName, lastName: lastName}})
+  .then(() => {
+    res.json({ message: 'OK' })
+  })
+  .catch((err) => {
+    res.status(500).json({ message: err })
+  })
+});
+
+router.delete('/employees/:id', (req, res) => {
+  req.db.collection('employees')
+  .deleteOne({ _id: ObjectId(req.params.id) })
+  .then(() => {
+    res.json({ message: 'OK' })
+  })
+  .catch((err) => {
+    res.status(500).json({ message: err });
+  })
+});
+
+module.exports = router;
